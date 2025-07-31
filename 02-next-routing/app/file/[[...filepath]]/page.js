72aa5d4f692662dea271ac5fe645b0fc0ddd6183
Musab <joiya4636@gmail.com>
2025-07-31 01:41:38 -0700

    routing

--- 02-next-routing/app/file/[[...filepath]]/page.js
+++ 02-next-routing/app/file/[[...filepath]]/page.js
@@ -6,7 +6,7 @@ export default async function Page({params}) {
 
     return (
         <>
-            <h1>File Path: {filepath.join('/')}</h1>
+            <h1>File Path: {filepath?.join('/')}</h1>
             <p>This is a dynamic route that captures all segments of the URL.</p>
             
         </>
