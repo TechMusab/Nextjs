d38aee69495c2ebe87ef11aa922a8590bd1e36f0
Musab <joiya4636@gmail.com>
2025-08-10 02:52:58 -0700

    Update page.js

--- 09-redux/src/app/page.js
+++ 09-redux/src/app/page.js
@@ -1,9 +1,15 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <h1>
-    Counter App
-   </h1>
+    <div className="flex flex-col gap-6 m-10">
+      <h1 className="font-bold text-3xl ">
+        Counter App
+      
+      </h1>
+      <Link className="bg-green-600 rounded-2xl px-6 py-2 w-40" href="/counter">
+          Go to Counter
+        </Link>
+    </div>
   );
 }
